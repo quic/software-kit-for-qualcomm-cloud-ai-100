3d6f198b7fc0589d9b13948d30ac7217c1cd313c
Zack McKevitt <zmckevit@qti.qualcomm.com>
2025-05-16 10:21:27 -0700

    Update copyright markings to the modern policy
    Move to using a yearless, Qualcomm Technologies Inc copyright
    instead of the existing dated Qualcomm Innovation Center copyrights.
    
    Signed-off-by: Zack McKevitt <zmckevit@qti.qualcomm.com>

--- lib/runtime-platform/inc/dev/aic100/QRuntimePlatformDeviceAic100.h
+++ lib/runtime-platform/inc/dev/aic100/QRuntimePlatformDeviceAic100.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 #ifndef QRUNTIME_PLATFORM_DEVICE_AIC100_H
 #define QRUNTIME_PLATFORM_DEVICE_AIC100_H
